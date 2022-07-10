63a12e3129f227419b7ba8a1337ddc33b2543e3a
Екатерина Калистратова <kalistratova_k@mail.ru>
2022-07-10 18:52:22 +0300

    API

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name; // поле для имени
+    public String passport;// поле для паспорта
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+}
